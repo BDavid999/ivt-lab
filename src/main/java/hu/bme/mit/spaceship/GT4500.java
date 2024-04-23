69c36d08d75806f411561e7326110399eb31dccc
BDavid9999ö <besenyi.david99@gmail.com>
2024-04-23 16:19:30 +0200

    Update GT4500.java

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -15,7 +15,7 @@ public class GT4500 implements SpaceShip {
     this.secondaryTorpedoStore = new TorpedoStore(10);
   }
 
-  public boolean fireLaser(FiringMode firingMode) { //branch-B edit {
+  public boolean fireLaser(FiringMode firingMode) {
     // TODO not implemented yet
     return false;
   }
